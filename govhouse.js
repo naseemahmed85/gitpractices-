d72848a60f7d18d8b6324f04e58264503ccd1f70
naseemahmed <nf693307@gmail.com>
2024-02-25 12:13:28 +0500

    variable

---
+++ govhouse.js
@@ -0,0 +1,6 @@
+var program = "Governor House";
+console.log(program);
+var program1 = "Kamran Khan Tessori";
+console.log(program1);
+var userName = "Sir Amin Alam";
+console.log(userName);
---
+++ govhouse.ts
@@ -0,0 +1,9 @@
+let program = "Governor House"
+console.log(program);
+
+let program1 = "Kamran Khan Tessori"
+console.log(program1);
+
+let userName: string ="Sir Amin Alam"
+console.log(userName);
+
