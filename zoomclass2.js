ca024888fb2ac394ef79d18bdabd8727bfa2638c
naseemahmed <nf693307@gmail.com>
2024-03-06 00:46:51 +0500

    zoomClass2

---
+++ zoomclass2.js
@@ -0,0 +1,15 @@
+if (true) {
+    var firstName = "Naseem";
+    var midName = "Ahmed";
+    var lastName = "Abbasi";
+    var fullName = ("".concat(firstName, " ").concat(midName, " ").concat(lastName));
+    console.log(fullName);
+}
+if (true) {
+    var myName = "Naseem Ahmed";
+    var myFatherName = "Sajawal Khan";
+    var mobileNumber = "+923161285016";
+    var bankName = "MCB";
+    var completeData = ("".concat(myName, " | ").concat(myFatherName, " | ").concat(mobileNumber, " | ").concat(bankName));
+    console.log(completeData);
+}
---
+++ zoomclass2.ts
@@ -0,0 +1,16 @@
+if (true) {
+    let firstName: string = "Naseem"
+    let midName: string = "Ahmed"
+    let lastName: string = "Abbasi"
+    let fullName: String = (`${firstName} ${midName} ${lastName}`)
+    console.log(fullName)
+}
+
+if (true) {
+    let myName: string = "Naseem Ahmed"
+    let myFatherName: string = "Sajawal Khan"
+    let mobileNumber: string = "+923161285016"
+    let bankName: string = "MCB"
+    let completeData = (`${myName} | ${myFatherName} | ${mobileNumber} | ${bankName}`)
+    console.log(completeData)
+}
